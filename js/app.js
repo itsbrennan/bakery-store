25858d0108bf78aa1d6209e96a4a98c98838275e
Brennan Martin <Brennan.Martin@arrow.com>
2019-06-20 14:28:27 -0600

    added modal functionality

--- js/app.js
+++ js/app.js
@@ -49,3 +49,67 @@
     });
   });
 })();
+
+
+// light box
+
+(function(){
+
+  let imageList = [];
+  let counter = 0;
+
+  const images = document.querySelectorAll('.store-img');
+  const container = document.querySelector('.lightbox-container');
+  const item = document.querySelector('.lightbox-item');
+  const closeIcon = document.querySelector('.lightbox-close');
+  const btnLeft = document.querySelector('.btnLeft');
+  const btnRight = document.querySelector('.btnRight');
+
+  // add all images to an array
+  images.forEach(function(img) {
+    imageList.push(img.src);
+
+  });
+  //console.log(imageList);
+
+  // open modal
+  images.forEach(function(img){
+    img.addEventListener('click', function(event){
+      // show modal
+      container.classList.add('show');
+      // get source
+      let src = event.target.src;
+      //console.log(src);
+      counter = imageList.indexOf(src);
+      // show modal with correct image
+      item.style.backgroundImage = `url(${src})`;
+      
+      
+      
+    })
+  })
+
+  // close modal
+  closeIcon.addEventListener('click', function(){
+    container.classList.remove('show');
+  })
+  // left button
+  btnLeft.addEventListener('click', function(){
+    counter --;  
+    if(counter < 0){
+      counter = imageList.length -1;
+    }
+    item.style.backgroundImage = `url(${imageList[counter]})`
+    console.log(counter);
+  })
+  // right button
+  btnRight.addEventListener('click', function(){
+    counter ++;  
+    if(counter > imageList.length -1){
+      counter = 0;
+    }
+    item.style.backgroundImage = `url(${imageList[counter]})`
+    console.log(counter);
+  })
+
+})();
